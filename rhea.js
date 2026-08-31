24e99e17eeb7dd61622e7043ee563a3e3bd12032
rheamaebayta-rgb <rheamaebayta@gmail.com>
2026-08-31 18:53:36 +0800

    Add files via upload

---
+++ rhea.js
@@ -0,0 +1,6 @@
+const contactBtn = document.getElementById("contactBtn");
+const message = document.getElementById("message");
+
+contactBtn.addEventListener("click", function () {
+    message.textContent = "Thank you for visiting my portfolio!";
+});
